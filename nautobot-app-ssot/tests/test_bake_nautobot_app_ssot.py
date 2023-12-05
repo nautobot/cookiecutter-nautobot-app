a92b60edac7ac627c8041a08f0a82ea4acd9b962
Jeremy White <jeremy.white@networktocode.com>
2023-12-04 20:57:01 -0500

    updates to standardize invoke

--- nautobot-app-ssot/tests/test_bake_nautobot_app_ssot.py
+++ nautobot-app-ssot/tests/test_bake_nautobot_app_ssot.py
@@ -23,6 +23,7 @@ def test_bake_nautobot_execution(cookies_baked_nautobot_app_ssot):
     Tests creation of example nautobot with the cookiecutter default values
     """
     results, examples_projects = cookies_baked_nautobot_app_ssot
+    print(results)
     plugin_slug = "nautobot-plugin-ssot-ext-sor"
     assert results[plugin_slug].exit_code == 0
     assert results[plugin_slug].exception is None
