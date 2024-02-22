0b4b2f2388bc651aceccb1c020d96d1e2eec9365
Jeremy White <jeremy.lee.w@me.com>
2024-02-22 11:09:44 -0500

    Only run linter formating

--- nautobot-app/{{ cookiecutter.project_slug }}/tasks.py
+++ nautobot-app/{{ cookiecutter.project_slug }}/tasks.py
@@ -689,7 +689,7 @@ def pylint(context):
 def autoformat(context):
     """Run code autoformatting."""
     black(context, autoformat=True)
-    ruff(context, action="both", fix=True)
+    ruff(context, fix=True)
 
 
 @task(
