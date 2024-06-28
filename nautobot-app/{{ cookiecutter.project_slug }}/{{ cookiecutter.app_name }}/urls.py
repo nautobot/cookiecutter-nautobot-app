cb44f5643ce5ce42e0975afeefa7de282fd50f5c
Josh VanDeraa <jv@networktocode.com>
2024-06-28 09:13:33 -0500

    Updates init and urls to handle docs page link activation.

--- nautobot-app/{{ cookiecutter.project_slug }}/{{ cookiecutter.app_name }}/urls.py
+++ nautobot-app/{{ cookiecutter.project_slug }}/{{ cookiecutter.app_name }}/urls.py
@@ -1,5 +1,9 @@
 """Django urlpatterns declaration for {{ cookiecutter.app_name }} app."""
 
+from django.urls import path
+from django.templatetags.static import static
+from django.views.generic import RedirectView
+
 from nautobot.apps.urls import NautobotUIViewSetRouter
 
 from {{ cookiecutter.app_name }} import views
@@ -7,4 +11,8 @@ from {{ cookiecutter.app_name }} import views
 router = NautobotUIViewSetRouter()
 router.register("{{ cookiecutter.model_class_name | lower }}", views.{{ cookiecutter.model_class_name }}UIViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("docs/", RedirectView.as_view(url=static("{{ cookiecutter.app_name }}/docs/index.html")), name="docs"),
+]
+
+urlpatterns += router.urls
