cd062084f721a2e5e9200b7d1c05032ecaf3ae24
Andrew Turner <aj.turner@live.com>
2024-07-09 11:15:13 -0500

    Update SSoT Adapter Test for NB 2.x

--- nautobot-app-ssot/{{ cookiecutter.project_slug }}/{{ cookiecutter.app_name }}/tests/test_{{ cookiecutter.system_of_record_slug }}_adapter.py
+++ nautobot-app-ssot/{{ cookiecutter.project_slug }}/{{ cookiecutter.app_name }}/tests/test_{{ cookiecutter.system_of_record_slug }}_adapter.py
@@ -32,7 +32,7 @@ class Test{{ cookiecutter.system_of_record_camel }}AdapterTestCase(TransactionTe
 
         self.job = {{ cookiecutter.system_of_record_camel }}DataSource()
         self.job.job_result = JobResult.objects.create(
-            name=self.job.class_path, obj_type=ContentType.objects.get_for_model(Job), user=None, job_id=uuid.uuid4()
+            name=self.job.class_path, user=None, id=uuid.uuid4()
         )
         self.{{ cookiecutter.system_of_record_slug }} = {{ cookiecutter.system_of_record_camel }}Adapter(job=self.job, sync=None, client=self.{{ cookiecutter.system_of_record_slug }}_client)
 
