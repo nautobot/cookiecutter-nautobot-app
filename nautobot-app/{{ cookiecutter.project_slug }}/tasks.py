e3773cc50dc1b64851e614826266865fba1659bd
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2024-10-07 12:37:57 -0700

    skip check if migrations directory does not exist

--- nautobot-app/{{ cookiecutter.project_slug }}/tasks.py
+++ nautobot-app/{{ cookiecutter.project_slug }}/tasks.py
@@ -710,21 +710,24 @@ def hadolint(context):
 def pylint(context):
     """Run pylint code analysis."""
     exit_code = 0
-    run_command(context, command)
 
     base_pylint_command = 'pylint --verbose --init-hook "import nautobot; nautobot.setup()" --rcfile pyproject.toml'
     command = f"{base_pylint_command} {{ cookiecutter.app_name }}"
     if not run_command(context, command, warn=True):
         exit_code = 1
 
-    # run the pylint_django migrations checkers on the migrations directory
-    migrations_pylint_command = (
-        f"{base_pylint_command} --load-plugins=pylint_django.checkers.migrations"
-        " --disable=all --enable=new-db-field-with-default,missing-backwards-migration-callable"
-        " {{ cookiecutter.app_name }}.migrations"
-    )
-    if not run_command(context, migrations_pylint_command, warn=True):
-        exit_code = 1
+    # run the pylint_django migrations checkers on the migrations directory, if one exists
+    migrations_dir = Path(__file__).absolute().parent / Path("{{ cookiecutter.app_name }}") / Path("migrations")
+    if migrations_dir.is_dir():
+        migrations_pylint_command = (
+            f"{base_pylint_command} --load-plugins=pylint_django.checkers.migrations"
+            " --disable=all --enable=fatal,new-db-field-with-default,missing-backwards-migration-callable"
+            " {{ cookiecutter.app_name }}.migrations"
+        )
+        if not run_command(context, migrations_pylint_command, warn=True):
+            exit_code = 1
+    else:
+        print("No migrations directory found, skipping migrations checks.")
 
     raise Exit(code=exit_code)
 
