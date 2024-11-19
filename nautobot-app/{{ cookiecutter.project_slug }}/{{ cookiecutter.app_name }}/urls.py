19be5739be113151fe7968c3612da1d051f068aa
Stephen Kiely <stephenkiely87@gmail.com>
2024-11-19 09:07:58 -0700

    Make urls.py a file that is always baked.

--- nautobot-app/{{ cookiecutter.project_slug }}/{{ cookiecutter.app_name }}/urls.py
+++ nautobot-app/{{ cookiecutter.project_slug }}/{{ cookiecutter.app_name }}/urls.py
@@ -5,10 +5,20 @@ from django.urls import path
 from django.views.generic import RedirectView
 from nautobot.apps.urls import NautobotUIViewSetRouter
 
+{% if cookiecutter.model_class_name != "None" %}
 from {{ cookiecutter.app_name }} import views
+{% else %}
+# Uncomment the following line if you have views to import
+# from {{ cookiecutter.app_name }} import views
+{% endif %}
 
 router = NautobotUIViewSetRouter()
+{% if cookiecutter.model_class_name != "None" %}
 router.register("{{ cookiecutter.model_class_name | lower }}", views.{{ cookiecutter.model_class_name }}UIViewSet)
+{% else %}
+# Here is an example of how to register a viewset, you will want to replace views.{{ cookiecutter.camel_name }}UIViewSet with your viewset
+# router.register("{{ cookiecutter.app_name }}", views.{{ cookiecutter.camel_name }}UIViewSet)
+{% endif %}
 
 urlpatterns = [
     path("docs/", RedirectView.as_view(url=static("{{ cookiecutter.app_name }}/docs/index.html")), name="docs"),
