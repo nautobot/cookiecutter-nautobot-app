96d19349c97f1b2991c288934af69f09d83e0b6e
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2025-08-19 10:19:52 -0700

    hard code tasks.py nautobot version

--- nautobot-app/{{ cookiecutter.project_slug }}/tasks.py
+++ nautobot-app/{{ cookiecutter.project_slug }}/tasks.py
@@ -52,7 +52,7 @@ namespace = Collection("{{ cookiecutter.app_name }}")
 namespace.configure(
     {
         "{{ cookiecutter.app_name }}": {
-            "nautobot_ver": "{{ cookiecutter.min_nautobot_version }}",
+            "nautobot_ver": "2.4.11",
             "project_name": "{{ cookiecutter.app_slug }}",
             "python_ver": "3.11",
             "local": False,
