a0f5f9d41e5860f57e78aab6d62feb1fef6a8c60
Chris Tomkins <chris.tomkins@networktocode.com>
2025-08-22 12:36:43 +0100

    feat: :sparkles: Add a new CustomOrderingDiff to support custom ordering

--- nautobot-app-ssot/{{ cookiecutter.project_slug }}/{{ cookiecutter.app_name }}/jobs.py
+++ nautobot-app-ssot/{{ cookiecutter.project_slug }}/{{ cookiecutter.app_name }}/jobs.py
@@ -3,6 +3,7 @@
 from nautobot.apps.jobs import BooleanVar, register_jobs
 from nautobot_ssot.jobs.base import DataSource, DataTarget
 
+from {{ cookiecutter.app_name }}.diff import CustomOrderingDiff
 from {{ cookiecutter.app_name }}.diffsync.adapters import {{ cookiecutter.system_of_record_camel }}RemoteAdapter, {{ cookiecutter.system_of_record_camel }}NautobotAdapter
 
 name = "{{ cookiecutter.system_of_record }} SSoT"  # pylint: disable=invalid-name
@@ -88,6 +89,16 @@ class {{ cookiecutter.system_of_record_camel }}DataTarget(DataTarget):
         self.dryrun = dryrun
         self.memory_profiling = memory_profiling
         super().run(dryrun=self.dryrun, memory_profiling=self.memory_profiling, *args, **kwargs)
+
+    def execute_sync(self):
+        """Method to synchronize the difference from `self.diff`, from SOURCE to TARGET adapter.
+
+        Overridden to use a CustomOrderingDiff diff_class.
+        """
+        if self.source_adapter is not None and self.target_adapter is not None:
+            self.source_adapter.sync_to(self.target_adapter, flags=self.diffsync_flags, diff_class=CustomOrderingDiff)
+        else:
+            self.logger.warning("One of the adapters was not properly initialized prior to synchronization.")
 
 
 jobs = [{{ cookiecutter.system_of_record_camel }}DataSource, {{ cookiecutter.system_of_record_camel }}DataTarget]
