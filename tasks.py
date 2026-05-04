b60e3694a7a83597e5f57fed0a1b2fc407921710
Noah Robin <noahr@nvidia.com>
2026-05-04 01:09:35 -0400

    Closes #373: Added 'target' and 'recursive' options to 'invoke pylint' tasks

--- tasks.py
+++ tasks.py
@@ -339,10 +339,19 @@ def generate_release_notes(context, version):
 # ------------------------------------------------------------------
 # TESTS
 # ------------------------------------------------------------------------------
-@task
-def pylint(context):
+@task(
+    help={
+        "target": "Module or file or directory to inspect, repeatable (default: project python files and template tests)",
+        "recursive": "Must be set if target is a directory rather than a module or file name",
+    },
+    iterable=["target"],
+)
+def pylint(context, target=None, recursive=False):
     """Run pylint code analysis."""
-    command = f"pylint --rcfile pyproject.toml {collect_files(context)}"
+    command = "pylint --rcfile pyproject.toml "
+    if recursive:
+        command += "--recursive=y "
+    command += " ".join(target) if target else collect_files(context)
     run_command(context, command)
 
 
